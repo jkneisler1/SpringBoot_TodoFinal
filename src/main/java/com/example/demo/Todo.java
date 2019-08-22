44b1644564655fabdeed8efb7610afba0d3083bd
GBTC405002UR <GBTC405002UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-22 14:18:23 -0400

    Todo project

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String title;
+
+    @Size(max=150)
+    private String content;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
